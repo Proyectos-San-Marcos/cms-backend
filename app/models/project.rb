0d0e411a95aa118c18cb69766723fdd3feedbafa
Joseph Vega <jcvb0206@gmail.com>
2021-07-26 10:19:58 -0500

    Update associations on models User, UserProject and Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,12 @@
 class Project < ApplicationRecord
+  has_many :user_projects, dependent: :destroy
+  has_many :users, through: :user_projects
+
+  # Self-Join
   has_many :editions, class_name: 'Project',
-                      foreign_key: 'parent_id'
+                      foreign_key: 'parent_id',
+                      dependent: :destroy,
+                      inverse_of: :parent
 
   belongs_to :parent, class_name: 'Project', optional: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   belongs_to :area
   belongs_to :career
   has_one_attached :photo
+  has_many :user_projects, dependent: :destroy
+  has_many :projects, through: :user_projects
 end
