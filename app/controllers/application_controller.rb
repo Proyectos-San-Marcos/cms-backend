96b565e504902654c87eb56d617eb294527fac3a
Joseph Vega <jcvb0206@gmail.com>
2021-07-28 15:44:02 -0500

    User can login, sign-up and logout (only members with area and career info - To edit this option -)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit :sign_up,
+                                      keys: %i[email username password first_name last_name area_id
+                                               career_id]
+  end
 end
